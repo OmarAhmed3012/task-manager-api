a28fce34f482eb169cd0eb4a466480a24d19e75c
OmarAhmed3012 <omaraboshamaa@gmail.com>
2020-03-04 02:25:00 +0200

    adajfadnfjk

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 //const {MONGODB_URL} = require('../../config/constrains')  //process.env.MONGODB_URL
 
-mongoose.connect("mongodb+srv://taskapp:omarahmed3012@cluster0-irrbg.mongodb.net/task-manager-api?retryWrites=true", {
+mongoose.connect('mongodb+srv://taskapp:omarahmed3012@cluster0-irrbg.mongodb.net/task-manager-api?retryWrites=true', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
--- src/models/user.js
+++ src/models/user.js
@@ -76,7 +76,7 @@ userSchema.methods.toJSON = function () {
 
 userSchema.methods.generatAuthToken = async function () {
     const user = this
-    const token = jwt.sign({ _id: user._id.toString() }, /*process.env.JWY_SECRET*/ 'omarahmed')
+    const token = jwt.sign({ _id: user._id.toString() }, process.env.JWY_SECRET /*'omarahmed'*/)
 
     user.tokens = user.tokens.concat({ token })
     await user.save()
