b4de2232e4c463a333617de8f6f4469c366f3048
OmarAhmed3012 <omaraboshamaa@gmail.com>
2020-03-12 00:22:04 +0200

    e.message

--- src/routers/user.js
+++ src/routers/user.js
@@ -23,9 +23,10 @@ router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
         const token = await user.generateAuthToken()
+        console.log(req.body)
         res.send({ user, token })
     } catch (e) {
-        res.status(400).send()
+        res.status(400).send(e.message)
     }
 })
 
