0aead2c6ab81525b91811b0d91c38e9ff11b79e4
OmarAhmed3012 <omaraboshamaa@gmail.com>
2020-03-11 14:19:09 +0200

    access control

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 //dfjn
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
 
 app.use(express.json())
 app.use(userRouter)
